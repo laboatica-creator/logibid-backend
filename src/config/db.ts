fa1f156c474964968c8ca1ec1ba25354beff2d53
laboatica-creator <laboatica@gmail.com>
2026-04-05 17:12:05 -0600

    Configurar conexión a base de datos con variable de entorno

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,11 +2,8 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: 'laboa',
-  password: 'LogiBid123',
-  host: 'localhost',
-  port: 5433,
-  database: 'logibid',
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 export { pool };
